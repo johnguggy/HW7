6bbeb041627db9fbd5abd56a98405e5d1ec50b3d
johnguggy <42472160+johnguggy@users.noreply.github.com>
2018-09-18 17:03:50 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    string pal = string.Empty;
+                    Console.WriteLine("Enter text to see if it's a palindrome:" + "(Enter q to quit)");
+                    pal = Console.ReadLine();
+
+                    if (pal.ToUpper().Equals("Q"))
+                        break;
+                    string NotPal = GetStringReversed(pal);
+                    
+                    if (pal.ToLower().Equals(NotPal.ToLower()))
+                    {
+                        Console.WriteLine("Palindrome");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a Palindrome");
+                    }
+                }
+            }
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("General Exception Thrown" + ex.Message);
+            }
+        }
+        public static string GetStringReversed(string input)
+        {
+            char[] sentence = input.ToCharArray();
+            Array.Reverse(sentence);
+            return new string(sentence);
+        }
+    }
+}
